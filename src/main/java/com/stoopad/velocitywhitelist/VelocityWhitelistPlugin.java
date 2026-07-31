ceb58c1c885da447c713ddff5b50745202ead1c4
HMwancheng <HM_wancheng@outlook.com>
2026-07-31 17:00:27 +0800

    fix: use com.google.inject.Inject and annotation-generated velocity-plugin.json

--- src/main/java/com/stoopad/velocitywhitelist/VelocityWhitelistPlugin.java
+++ src/main/java/com/stoopad/velocitywhitelist/VelocityWhitelistPlugin.java
@@ -4,18 +4,22 @@ import com.stoopad.velocitywhitelist.listener.BotCommandListener;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
 import org.slf4j.Logger;
 
-import javax.inject.Inject;
+import com.google.inject.Inject;
 import java.nio.charset.StandardCharsets;
 
 @Plugin(
         id = "huhostdwhitelist",
         name = "HuHoSTDWhiteList-Velocity",
         version = "1.0.0",
-        authors = {"stoopad"}
+        authors = {"stoopad"},
+        dependencies = {
+                @Dependency(id = "huhobot", optional = false)
+        }
 )
 public class VelocityWhitelistPlugin {
 
